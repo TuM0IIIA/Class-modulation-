61716b05f85180cb2cd93fb41db4cc584ec9a063
TuM0IIIA <Timofey.Posled@gmail.com>
2022-08-20 20:32:48 +0300

    a collector module that combines all in a single namespace

---
+++ lister.py
@@ -0,0 +1,9 @@
+# Файл lister.ру
+
+# Для удобства собирает в одном модуле все три класса, выводящие атрибуты
+
+
+from listinstance import ListInstance
+from listtree import ListTree
+
+Lister = ListTree  # Выбрать стандартный класс, выводящий атрибуты
