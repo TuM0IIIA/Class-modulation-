3908b15a1058c0a42ca6064aff91f54d122fa4be
Tim <64282785+TuM0IIIA@users.noreply.github.com>
2022-08-20 20:34:29 +0300

    Delete lister.py

--- lister.py
+++
@@ -1,9 +0,0 @@
-# Файл lister.ру
-
-# Для удобства собирает в одном модуле все три класса, выводящие атрибуты
-
-
-from listinstance import ListInstance
-from listtree import ListTree
-
-Lister = ListTree  # Выбрать стандартный класс, выводящий атрибуты
